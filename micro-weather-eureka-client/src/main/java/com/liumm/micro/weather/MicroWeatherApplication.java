8aa266e87657b958a29496d811ace0efbd85e586
刘明明 <liumm@mac.local>
2018-03-24 22:36:24 +0800

    服务注册于发现

---
+++ micro-weather-eureka-client/src/main/java/com/liumm/micro/weather/MicroWeatherApplication.java
@@ -0,0 +1,22 @@
+package com.liumm.micro.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 
+ * @ClassName: MicroWeatherApplication 
+ * @Description: TODO(这里用一句话描述这个类的作用) 
+ * @author liumm 
+ * @date 2018年3月24日 下午8:53:01 
+ *
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MicroWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroWeatherApplication.class, args);
+	}
+}
