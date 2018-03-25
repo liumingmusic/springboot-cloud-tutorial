a5de6c162e437436e4850dce50b05adc9ead0bb0
刘明明 <liumm@mac.local>
2018-03-25 16:11:21 +0800

    服务的注册与发现

--- msa-weather-collection-eureka/src/main/java/com/liumm/msa/weather/MicroWeatherCollectionApplication.java
+++ msa-weather-collection-eureka/src/main/java/com/liumm/msa/weather/MicroWeatherCollectionApplication.java
@@ -2,6 +2,7 @@ package com.liumm.msa.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 
@@ -12,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroWeatherCollectionApplication {
 
 	public static void main(String[] args) {
--- msa-weather-report-eureka/src/main/java/com/liumm/msa/weather/vo/City.java
+++ msa-weather-report-eureka/src/main/java/com/liumm/msa/weather/vo/City.java
@@ -8,11 +8,6 @@
  */
 package com.liumm.msa.weather.vo;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlRootElement;
-
 /** 
  * @ClassName: City 
  * @Description: 城市数据信息
